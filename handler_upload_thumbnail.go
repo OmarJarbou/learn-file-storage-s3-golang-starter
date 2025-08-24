96ace8365bbf5c3d16f6309c615e477c384af7a7
Omar Jarbou <omarjarbou2019@gmail.com>
2025-08-24 22:04:14 +0300

    Implement thumbnail upload functionality in handler_upload_thumbnail.go, including video ownership validation, file(thummbnail) parsing, adding file(thumbnail) to the corresoinding map,  video metadata update, and returning the updated video metadata.

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,55 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	vedioMetadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error fetching vedio with this id", err)
+		return
+	}
+	if vedioMetadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You are not the owner of this video!", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// Set a const maxMemory to 10MB
+	const maxMemory = 10 << 20 // bit-shifted the number 10 to the left 20 times to get an int that stores the proper number of bytes
+	//Bit shifting is a way to multiply by powers of 2. 10 << 20 is the same as 10 * 1024 * 1024, which is 10MB.
+	r.ParseMultipartForm(maxMemory)
+
+	// "thumbnail" should match the HTML form input name
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	// The Content-Type is in the header.Header map, not via Get method
+	mediaType := header.Header.Get("Content-Type")
+	data, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error reading thumbnail file content", err)
+		return
+	}
+
+	thumb := thumbnail{
+		data:      data,
+		mediaType: mediaType,
+	}
+
+	videoThumbnails[videoID] = thumb
+
+	// Update video metadata to contain thumbnail url
+	thumbnailURL := "http://localhost:" + cfg.port + "/api/thumbnails/" + videoID.String()
+	vedioMetadata.ThumbnailURL = &thumbnailURL
+	err = cfg.db.UpdateVideo(vedioMetadata)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error updating video to contain thumbnail url", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, vedioMetadata)
 }
